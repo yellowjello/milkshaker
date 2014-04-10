6d19ebc44fd81e9b58acd33d15c9dff5552aa5e5
Henry Chee <mr.pointless@gmail.com>
2014-04-10 12:48:29 -0700

    Turbulence

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,16 +1,36 @@
 function GameManager() {
 	this.milk = 0;
-	this.items = {};
+	this.mv = 0;
+	//this.items = {};
 	// {"itemName": {"cost": cost, "max": amount}}
-	this.ITEM_COSTS = {"mv": {"cost": 50}, "thing": {"cost": 50, "max": 2}}
+	//this.ITEM_COSTS = {"mv": {"cost": 50}, "thing": {"cost": 50, "max": 2}}
 	this.callbacks = [];
+	this.turbulence = 0;
+	this.MAX_TURBULENCE = 100;
 }
+
+GameManager.prototype.shakeMilk = function(milk) {
+	this.turbulence += milk;
+	if (this.turbulence > this.MAX_TURBULENCE) {
+		this.turbulence = this.MAX_TURBULENCE;
+	}
+	this.addMilk(milk);
+};
 
 GameManager.prototype.addMilk = function(milk) {
 	this.milk += milk;
 	this.notifyUpdate();
 };
 
+GameManager.prototype.decreaseTurbulence = function(turbulence) {
+	this.turbulence -= turbulence;
+	if (this.turbulence < 0) {
+		this.turbulence = 0;
+	}
+	this.notifyUpdate();
+};
+
+/*
 GameManager.prototype.purchase = function(item, callback) {
 	if (this.canPurchase(item)) {
 		// Subtract milk
@@ -30,6 +50,7 @@ GameManager.prototype.canPurchase = function(item) {
 	var belowLimit = !this.ITEM_COSTS[item]["max"] || !this.items[item] || this.items[item] < this.ITEM_COSTS[item]["max"];
 	return enoughMilk && belowLimit;
 };
+*/
 
 GameManager.prototype.addUpdateListener = function(callback) {
 	this.callbacks.push(callback);
